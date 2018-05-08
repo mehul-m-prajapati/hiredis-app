a32c2269d8403eb906acebede28e821383489199
Mehul Prajapati <mehulprajapati2802@gmail.com>
2018-05-08 15:56:31 +0530

    Re-arranging the code

--- hiredis_app.c
+++ hiredis_app.c
@@ -1,35 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include </usr/include/hiredis/hiredis.h>
 
-int main(int argc, char **argv) {
+#define HOST_ADDR		"127.0.0.1"
+#define HOST_PORT_NO		6379
+
+
+int main(int argc, char **argv) 
+{
     unsigned int j;
     redisContext *c;
     redisReply *reply;
-    const char *hostname = (argc > 1) ? argv[1] : "127.0.0.1";
-    int port = (argc > 2) ? atoi(argv[2]) : 6379;
+    const char *hostname = (argc > 1) ? argv[1] : HOST_ADDR;
+    int port = (argc > 2) ? atoi(argv[2]) : HOST_PORT_NO;
 
     struct timeval timeout = { 1, 500000 }; // 1.5 seconds
+
+    /* Connect to Server */
     c = redisConnectWithTimeout(hostname, port, timeout);
-    if (c == NULL || c->err) {
-        if (c) {
+    
+    if (c == NULL || c->err) 
+    {
+        if (c) 
+	{
             printf("Connection error: %s\n", c->errstr);
             redisFree(c);
-        } else {
+        } 
+	else 
+	{
             printf("Connection error: can't allocate redis context\n");
         }
         exit(1);
     }
 
     /* PING server */
-    reply = redisCommand(c,"PING");
+    reply = redisCommand(c, "PING");
     printf("PING: %s\n", reply->str);
     freeReplyObject(reply);
 
     /* Set a key */
-    reply = redisCommand(c,"SET %s %s", "foo", "hello world");
+    reply = redisCommand(c, "SET %s %s", "project", "syslog-ng");
     printf("SET: %s\n", reply->str);
     freeReplyObject(reply);
 
@@ -39,8 +50,8 @@ int main(int argc, char **argv) {
     freeReplyObject(reply);
 
     /* Try a GET and two INCR */
-    reply = redisCommand(c,"GET foo");
-    printf("GET foo: %s\n", reply->str);
+    reply = redisCommand(c,"GET project");
+    printf("GET project: %s\n", reply->str);
     freeReplyObject(reply);
 
     reply = redisCommand(c,"INCR counter");
@@ -54,7 +65,9 @@ int main(int argc, char **argv) {
     /* Create a list of numbers, from 0 to 9 */
     reply = redisCommand(c,"DEL mylist");
     freeReplyObject(reply);
-    for (j = 0; j < 10; j++) {
+    
+    for (j = 0; j < 10; j++) 
+    {
         char buf[64];
 
         snprintf(buf,64,"%u",j);
@@ -64,8 +77,11 @@ int main(int argc, char **argv) {
 
     /* Let's check what we have inside the list */
     reply = redisCommand(c,"LRANGE mylist 0 -1");
-    if (reply->type == REDIS_REPLY_ARRAY) {
-        for (j = 0; j < reply->elements; j++) {
+    
+    if (reply->type == REDIS_REPLY_ARRAY) 
+    {
+        for (j = 0; j < reply->elements; j++) 
+	{
             printf("%u) %s\n", j, reply->element[j]->str);
         }
     }
